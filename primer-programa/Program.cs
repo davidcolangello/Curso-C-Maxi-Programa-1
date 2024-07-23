0bb6b1d877348bd3b0557926c2fd8a5b9ad49d7b
unknown <david.colangello@gmail.com>
2024-07-23 01:31:01 -0300

    primera subida

---
+++ primer-programa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace   primer_programa{
+class Program
+{
+    static bool EsPar(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            return true ;
+        }
+        else
+        {
+            return false ;
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        bool esPar = EsPar(numero);
+
+        if (esPar == true)
+        {
+            Console.WriteLine("El número ingresado es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado es impar.");
+        }
+    }
+}}
